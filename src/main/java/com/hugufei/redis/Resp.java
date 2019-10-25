4deb48be26ca9acef28a5b15f4dde6e84d5005ae
hugufei <>
2019-10-26 01:56:08 +0800

    添加io相关学习demo

---
+++ src/main/java/com/hugufei/redis/Resp.java
@@ -0,0 +1,15 @@
+package com.hugufei.redis;
+
+public class Resp {
+
+    public static final String star = "*";
+    public static final String crlf = "\r\n";
+    public static final String lengthStart = "$";
+
+
+    public static enum command {
+        SET, GET, INCR
+    }
+
+
+}
